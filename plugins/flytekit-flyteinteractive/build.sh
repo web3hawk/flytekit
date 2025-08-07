032e4cd6998096ad919d520918b2bf6edb9d747a
jiaql <jiaql@founcer.com>
2025-08-07 17:24:38 +0800

    Remove unused build argument `VERSION` from Docker build script

--- plugins/flytekit-flyteinteractive/build.sh
+++ plugins/flytekit-flyteinteractive/build.sh
@@ -19,7 +19,6 @@ echo "Using build version: ${BUILD_VERSION}"
 
 # Build the Docker image
 docker build \
-    --build-arg VERSION=flyteinteractive-latest \
     --build-arg TARGETARCH=amd64 \
     -t ${IMAGE_NAME}:${VERSION} \
     -t ${IMAGE_NAME}:latest \
